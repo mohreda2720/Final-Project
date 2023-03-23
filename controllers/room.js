be872c9a156edc9e34136ab283285d80469501dc
“[Malak <[eng.malak.mr@gmail.com]>
2023-03-23 15:18:05 +0200

    changes by Malak-Romany

--- controllers/room.js
+++ controllers/room.js
@@ -1,28 +1,27 @@
-const fs=require('fs');
-var express=require('express') 
-const roomsModel=require('../models/Rooms')
+const fs = require('fs');
+var express = require('express')
+const roomsModel = require('../models/Rooms')
 const dotenv = require('dotenv')
 dotenv.config({ path: 'config.env' })
 
 
-function createroom(room){
-return  roomsModel.create(room)
+function createroom(room) {
+    return roomsModel.create(room)
 }
-   
-function getroom(id)
-{
+
+function getroom(id) {
     return roomsModel.find(id);
 }
 
-   
+
 function deleteroom(id)
+console.log('malak-romany')
 {
     return roomsModel.findByIdAndDelete(id);
 }
-  
-function updateroom(id,room)
-{
-    return roomsModel.findByIdAndUpdate(id,room);
+
+function updateroom(id, room) {
+    return roomsModel.findByIdAndUpdate(id, room);
 }
 
-module.exports={createroom,getroom,deleteroom,updateroom}
+module.exports = { createroom, getroom, deleteroom, updateroom }
